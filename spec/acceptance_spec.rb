73b866b6e0631dcccf3d07c0a9c0ae8ccc17e418
Josh Cheek <josh.cheek@gmail.com>
2016-03-01 18:57:57 -0600

    Set the acceptance test to run against our server... Doltata

--- spec/acceptance_spec.rb
+++ spec/acceptance_spec.rb
@@ -1,15 +1,23 @@
 require_relative '../app'
 require 'net/http'
 require 'rack'
+require 'doltata'
 
 RSpec.describe 'acceptance' do
   it 'serves my double app' do
     thread = Thread.new do
       Thread.abort_on_exception = true
-      Rack::Handler.default.run(DoubleApp, Port: 9299, Host: 'localhost')
+      Doltata.run(DoubleApp, Port: 9299, Host: 'localhost')
     end
-    sleep 1
-    body = Net::HTTP.get(URI('http://localhost:9299'))
+
+    body = nil
+    until body
+      begin
+        body = Net::HTTP.get(URI('http://localhost:9299'))
+      rescue Errno::ECONNREFUSED
+        # O.o
+      end
+    end
     expect(body).to include 'form'
 
     response = Net::HTTP.post_form(URI('http://localhost:9299/result'),
