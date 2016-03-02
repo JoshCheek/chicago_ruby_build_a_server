f66874f69896dde941be984c5c27b230f23a61ef
Josh Cheek <josh.cheek@gmail.com>
2016-03-01 19:35:08 -0600

    Prints the response

--- lib/doltata.rb
+++ lib/doltata.rb
@@ -14,14 +14,23 @@ class Doltata
     port   = @options[:Port]
     host   = @options[:Host]
     server = TCPServer.new(host, port)
-    socket = server.accept
-    env    = Doltata.parsy_thing(socket)
-    # get the request
-    # parse it
-    # hand it to the app
-    # app gives me response data
-    # send that back through the socket
+    loop do
+      socket = server.accept
+      env    = Doltata.parsy_thing(socket)
+      status, headers, body = @app.call(env)
+      Doltata.printy_thing(socket, status, headers, body)
+      socket.close
+    end
   end
+end
+
+def Doltata.printy_thing(socket, status, headers, body)
+  socket.print "HTTP/1.1 #{status} some text goes here *shrug*\r\n"
+  headers.each do |key, value|
+    socket.print "#{key}: #{value}\r\n"
+  end
+  socket.print "\r\n"
+  body.each { |line| socket.print line }
 end
 
 require 'stringio'
@@ -31,7 +40,6 @@ def Doltata.parsy_thing(socket)
   env['REQUEST_METHOD'] = method
   env['PATH_INFO']      = path
   env['HTTP_VERSION']   = version
-
   loop do
     key, value = socket.gets.chomp.split(": ")
     break unless key
@@ -39,9 +47,7 @@ def Doltata.parsy_thing(socket)
     key = "HTTP_#{key}" unless key == 'CONTENT_TYPE' || key == 'CONTENT_LENGTH'
     env[key] = value
   end
-
   body = socket.read env['CONTENT_LENGTH'].to_i
   env['rack.input'] = StringIO.new(body)
-
   env
 end
